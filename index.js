2326bb650d44fd2388d9f98c87b48ba8b80d1e88
Mohammad Furqan <101380683+furqan5921@users.noreply.github.com>
2025-04-14 20:12:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8001;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
